6ea7ba3df6dbeff374740bb83b6e9897dcd0ece1
Anders Ohma <anoh@norceresearch.no>
2026-01-23 17:00:38 +0100

    fixup

--- src/pargo/workflowgroup.py
+++ src/pargo/workflowgroup.py
@@ -63,14 +63,12 @@ class WorkflowGroup(BaseModel):
         for ind, workflows in enumerate(self._workflows):
             step = []
             for workflow in workflows:
-                arguments = (
-                    {
-                        "parameters": [
-                            {"name": k, "value": dumps(v), "default": dumps(v)}
-                            for k, v in workflow.parameters.items()
-                        ]
-                    },
-                )
+                arguments = {
+                    "parameters": [
+                        {"name": k, "value": dumps(v), "default": dumps(v)}
+                        for k, v in workflow.parameters.items()
+                    ]
+                }
                 step.append(
                     Task(
                         name=f"step-{ind}-{workflow.name}",
